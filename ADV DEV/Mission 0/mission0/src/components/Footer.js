db2f2ee01200516594024b23a0e6b360475c2472
JordanR-img <jordanrobin18@gmail.com>
2022-11-14 20:40:04 +1300

    adding in the footer element

--- ADV DEV/Mission 0/mission0/src/components/Footer.js
+++ ADV DEV/Mission 0/mission0/src/components/Footer.js
@@ -1,12 +1,13 @@
 import Style from "../Styling/Footer.module.css";
-
 export default function footer() {
   return (
     <div className={Style.FooterContainer}>
       <div className={Style.FooterItems}>
-        <div>Step 1: Identify food you are craving.</div>
-        <div>Step 2: Put the Food item into the search bar</div>
-        <div>Step 3: Pick a restaurant that has your desired craving</div>
+        <div className={Style.CravingContainer}><span className={Style.CravingText}>Step 1: Identify food you are craving.</span></div>
+        <div className={Style.CravingContainer}><span className={Style.CravingText}>Step 2: Put the food item in the search</span></div>
+        <div className={Style.CravingContainer}><span className={Style.CravingText}>Step 3: Pick restaurant that has your desired craving</span></div>
+        {/* <div>Step 2: Put the Food item into the search bar</div>
+        <div>Step 3: Pick a restaurant that has your desired craving</div> */}
       </div>
     </div>
   );

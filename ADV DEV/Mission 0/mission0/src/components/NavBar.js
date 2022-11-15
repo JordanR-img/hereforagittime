073cfad4a0c494df0be8af5e8ad3eb784ead4f69
JordanR-img <jordanrobin18@gmail.com>
2022-11-15 20:13:38 +1300

    continuing to make the page responsive

--- ADV DEV/Mission 0/mission0/src/components/NavBar.js
+++ ADV DEV/Mission 0/mission0/src/components/NavBar.js
@@ -1,7 +1,21 @@
 import Style from "../Styling/NavBar.module.css";
 import logo from '../public/FoodFinder.png'
+import useState from 'react';
+
 
 export default function NavBar() {
+  // const [nav, hideNav] = useState(true);
+
+  // function HamBurger(){
+  //   if (Style.Menu && Style.Login === 'none'){
+      
+  //   }
+  // }
+
+
+
+
+
   return (
     <div className={Style.NavBarContainer}>
       <div className={Style.NavBarItems}>
@@ -17,6 +31,7 @@ export default function NavBar() {
           <li className={Style.Menu}>Bug Report</li>
           <li className={Style.Menu}>Contact</li>
           <li className={Style.Login}>Login</li>
+          <span className={Style.Hamburger}>Button</span>
         </ul>
         </div>
       </div>

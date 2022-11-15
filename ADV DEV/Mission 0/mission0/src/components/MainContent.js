8d5c1a34d0fd63b5be26e3ad7f439762fd8076af
JordanR-img <jordanrobin18@gmail.com>
2022-11-15 17:25:05 +1300

    making media queries

--- ADV DEV/Mission 0/mission0/src/components/MainContent.js
+++ ADV DEV/Mission 0/mission0/src/components/MainContent.js
@@ -4,11 +4,13 @@ import Style from '../Styling/MainContent.module.css'
 export default function MainContent() {
   return (
     <div className={Style.MainContainer}>
+      <div className={Style.MainContainerItems}>
       <div className={Style.h1}>
         <h1>What are you craving?</h1>
       </div>
       <div className={Style.FormContainer}>
-        <form className={Style.FormItems}><textarea rows="4" cols="" type="text"/><label className={Style.Search}>Search</label></form>
+        <form className={Style.FormItems}><textarea rows="4" cols="" type="text" className={Style.textarea}/><label className={Style.Search}>Search</label></form>
+      </div>
       </div>
     </div>
   );

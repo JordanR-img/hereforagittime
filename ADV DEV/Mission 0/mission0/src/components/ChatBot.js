df4e959c7f8b60419380319227419024b281ff26
JordanR-img <jordanrobin18@gmail.com>
2022-11-22 22:41:14 +1300

    replaced and improved chatbot

--- ADV DEV/Mission 0/mission0/src/components/ChatBot.js
+++ ADV DEV/Mission 0/mission0/src/components/ChatBot.js
@@ -7,13 +7,23 @@ import Style from '../Styling/ChatBot.module.css';
 export default function chatBot() {
 
 return (
-    <div className={Style.ChatBotContainer}><iframe className={Style.ChatBot}
-    allow="microphone;"
-    width="350"
-    height="430"
-    src="https://console.dialogflow.com/api-client/demo/embedded/6628382f-3f62-435a-a816-175416050f49">
+//     <div className={Style.ChatBotContainer}><iframe className={Style.ChatBot}
+//     allow="microphone;"
+//     width="350"
+//     height="430"
+//     src="https://console.dialogflow.com/api-client/demo/embedded/6628382f-3f62-435a-a816-175416050f49">
       
-</iframe></div>
+// </iframe></div>
+
+<div>
+<df-messenger
+  intent="WELCOME"
+  chat-title="Appointment-Scheduler"
+  agent-id="6628382f-3f62-435a-a816-175416050f49"
+  language-code="en"
+  
+></df-messenger>
+</div>
 
 )
 }
